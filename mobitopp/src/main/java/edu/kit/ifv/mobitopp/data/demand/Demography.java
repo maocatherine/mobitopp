ce032bc180f16201f71d79f4193cb1918fd53e23
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 17:17:53 +1100

    Reformat

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/demand/Demography.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/demand/Demography.java
@@ -13,115 +13,116 @@ import edu.kit.ifv.mobitopp.simulation.Gender;
 
 public class Demography implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final EmploymentDistribution employment;
-  private final Map<AttributeType, RangeDistributionIfc> rangeDistributions;
+    private final EmploymentDistribution employment;
+    private final Map<AttributeType, RangeDistributionIfc> rangeDistributions;
 
-  public Demography(
-      EmploymentDistribution employment,
-      Map<AttributeType, RangeDistributionIfc> rangeDistributions) {
-    super();
-    this.employment = employment;
-    this.rangeDistributions = new LinkedHashMap<>(rangeDistributions);
-  }
-
-  public EmploymentDistribution employment() {
-    return employment;
-  }
-
-  public RangeDistributionIfc household() {
-    return getDistribution(StandardAttribute.householdSize);
-  }
-
-  public RangeDistributionIfc femaleAge() {
-    return getDistribution(StandardAttribute.femaleAge);
-  }
-
-  public RangeDistributionIfc maleAge() {
-    return getDistribution(StandardAttribute.maleAge);
-  }
-
-  /**
-   * May return <code>null</code>.
-   * @param type
-   * @return
-   */
-  public RangeDistributionIfc getDistribution(AttributeType type) {
-  	if (!rangeDistributions.containsKey(type)) {
-  		rangeDistributions.put(type, new RangeDistribution());
-  	}
-    return rangeDistributions.get(type);
-  }
-
-  public RangeDistributionIfc income() {
-    return getDistribution(StandardAttribute.income);
-  }
-  
-  public boolean hasData() {
-  	return rangeDistributions.values().stream().anyMatch(RangeDistributionIfc::hasItems);
-  }
-
-  public void increment(AttributeType type, int value) {
-    RangeDistributionIfc distribution = getDistribution(type);
-    if (!distribution.hasItem(value)) {
-      distribution.addItem(new RangeDistributionItem(value, 0));
+    public Demography(
+            EmploymentDistribution employment,
+            Map<AttributeType, RangeDistributionIfc> rangeDistributions) {
+        super();
+        this.employment = employment;
+        this.rangeDistributions = new LinkedHashMap<>(rangeDistributions);
     }
-    distribution.increment(value);
-  }
 
-  public void incrementHousehold(int type) {
-    household().increment(type);
-  }
-
-  public void incrementEmployment(Employment employment) {
-    this.employment.getItem(employment).increment();
-  }
-
-  public void incrementAge(Gender gender, int age) {
-    if (Gender.MALE == gender) {
-      increment(StandardAttribute.maleAge, age);
-    } else {
-      increment(StandardAttribute.femaleAge, age);
+    public EmploymentDistribution employment() {
+        return employment;
     }
-  }
 
-  public Demography createEmpty() {
-    EmploymentDistribution emptyEmployment = employment.createEmpty();
-    Map<AttributeType, RangeDistributionIfc> emptyDistributions = emptyDistributions();
-    return new Demography(emptyEmployment, emptyDistributions);
-  }
-
-  private Map<AttributeType, RangeDistributionIfc> emptyDistributions() {
-    LinkedHashMap<AttributeType, RangeDistributionIfc> emptyDistributions = new LinkedHashMap<>();
-    for (Entry<AttributeType, RangeDistributionIfc> entry : rangeDistributions.entrySet()) {
-      emptyDistributions.put(entry.getKey(), entry.getValue().createEmpty());
+    public RangeDistributionIfc household() {
+        return getDistribution(StandardAttribute.householdSize);
     }
-    return emptyDistributions;
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(employment, rangeDistributions);
-  }
+    public RangeDistributionIfc femaleAge() {
+        return getDistribution(StandardAttribute.femaleAge);
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Demography other = (Demography) obj;
-    return Objects.equals(employment, other.employment)
-        && Objects.equals(rangeDistributions, other.rangeDistributions);
-  }
+    public RangeDistributionIfc maleAge() {
+        return getDistribution(StandardAttribute.maleAge);
+    }
 
-  @Override
-  public String toString() {
-    return "Demography [employment=" + employment + ", rangeDistributions=" + rangeDistributions
-        + "]";
-  }
+    /**
+     * May return <code>null</code>.
+     *
+     * @param type
+     * @return
+     */
+    public RangeDistributionIfc getDistribution(AttributeType type) {
+        if (!rangeDistributions.containsKey(type)) {
+            rangeDistributions.put(type, new RangeDistribution());
+        }
+        return rangeDistributions.get(type);
+    }
+
+    public RangeDistributionIfc income() {
+        return getDistribution(StandardAttribute.income);
+    }
+
+    public boolean hasData() {
+        return rangeDistributions.values().stream().anyMatch(RangeDistributionIfc::hasItems);
+    }
+
+    public void increment(AttributeType type, int value) {
+        RangeDistributionIfc distribution = getDistribution(type);
+        if (!distribution.hasItem(value)) {
+            distribution.addItem(new RangeDistributionItem(value, 0));
+        }
+        distribution.increment(value);
+    }
+
+    public void incrementHousehold(int type) {
+        household().increment(type);
+    }
+
+    public void incrementEmployment(Employment employment) {
+        this.employment.getItem(employment).increment();
+    }
+
+    public void incrementAge(Gender gender, int age) {
+        if (Gender.MALE == gender) {
+            increment(StandardAttribute.maleAge, age);
+        } else {
+            increment(StandardAttribute.femaleAge, age);
+        }
+    }
+
+    public Demography createEmpty() {
+        EmploymentDistribution emptyEmployment = employment.createEmpty();
+        Map<AttributeType, RangeDistributionIfc> emptyDistributions = emptyDistributions();
+        return new Demography(emptyEmployment, emptyDistributions);
+    }
+
+    private Map<AttributeType, RangeDistributionIfc> emptyDistributions() {
+        LinkedHashMap<AttributeType, RangeDistributionIfc> emptyDistributions = new LinkedHashMap<>();
+        for (Entry<AttributeType, RangeDistributionIfc> entry : rangeDistributions.entrySet()) {
+            emptyDistributions.put(entry.getKey(), entry.getValue().createEmpty());
+        }
+        return emptyDistributions;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employment, rangeDistributions);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Demography other = (Demography) obj;
+        return Objects.equals(employment, other.employment)
+                && Objects.equals(rangeDistributions, other.rangeDistributions);
+    }
+
+    @Override
+    public String toString() {
+        return "Demography [employment=" + employment + ", rangeDistributions=" + rangeDistributions
+                + "]";
+    }
 
 }
