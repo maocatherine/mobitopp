8e93cc7437145d29e7503fa43757acc0bb65b7ef
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 16:34:27 +1100

    Inconsistency found. Use 'license'.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/util/panel/PersonInfo.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/util/panel/PersonInfo.java
@@ -5,26 +5,27 @@ import lombok.ToString;
 @ToString
 class PersonInfo {
 
-	public int person_number;
-	public int sex;
-	public int graduation;
-	public int birthYear;
-	public int employmentType;
-	public float distanceWork;
-	public float distanceEducation;
-	public int income = -1;
-	public boolean commutationTicket;
-	public boolean licence;
-	
-	//modetypeweights
-	public boolean fahrrad;
-	public boolean apkwverf;
-	public boolean ppkwverf;
+    public int person_number;
+    public int sex;
+    public int graduation;
+    public int birthYear;
+    public int age;
+    public int employmentType;
+    public float distanceWork;
+    public float distanceEducation;
+    public int income = -1;
+    public boolean commutationTicket;
+    public boolean license;
 
-	public float pref_cardriver;
-	public float pref_carpassenger;
-	public float pref_walking;
-	public float pref_cycling;
-	public float pref_publictransport;
+    //modetypeweights
+    public boolean fahrrad;
+    public boolean apkwverf;
+    public boolean ppkwverf;
+
+    public float pref_cardriver;
+    public float pref_carpassenger;
+    public float pref_walking;
+    public float pref_cycling;
+    public float pref_publictransport;
 
 }
