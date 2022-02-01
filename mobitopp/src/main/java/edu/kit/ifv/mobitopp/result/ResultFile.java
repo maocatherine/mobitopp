00142d2bdd0e98735cfc488e63b4269f42c2a162
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 16:25:41 +1100

    Reformat

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/result/ResultFile.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/result/ResultFile.java
@@ -18,50 +18,50 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ResultFile implements ResultOutput, Closeable {
 
-	static final String extension = ".csv";
+    static final String extension = ".csv";
 
-	private final BufferedWriter writer;
+    private final BufferedWriter writer;
 
-	ResultFile(BufferedWriter writer) {
-		super();
-		this.writer = writer;
-	}
+    ResultFile(BufferedWriter writer) {
+        super();
+        this.writer = writer;
+    }
 
-	@Override
-	public void writeLine(String text) throws UncheckedIOException {
-		try {
-			writer.write(text);
-			writer.newLine();
-			writer.flush();
-		} catch (IOException cause) {
-			throw warn(new UncheckedIOException(cause), log);
-		}
-	}
+    @Override
+    public void writeLine(String text) throws UncheckedIOException {
+        try {
+            writer.write(text);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException cause) {
+            throw warn(new UncheckedIOException(cause), log);
+        }
+    }
 
-	public static ResultFile create(File atBaseFolder, Category category) throws UncheckedIOException {
-		BufferedWriter output = writerFor(category, atBaseFolder);
-		ResultFile resultFile = new ResultFile(output);
-		resultFile.writeLine(category.header());
-		return resultFile;
-	}
+    public static ResultFile create(File atBaseFolder, Category category) throws UncheckedIOException {
+        BufferedWriter output = writerFor(category, atBaseFolder);
+        ResultFile resultFile = new ResultFile(output);
+        resultFile.writeLine(category.header());
+        return resultFile;
+    }
 
-	private static BufferedWriter writerFor(Category category, File baseFolder)
-			throws UncheckedIOException {
-		Path path = new File(baseFolder, fileName(category)).toPath();
-		try {
-			return Files.newBufferedWriter(path, TRUNCATE_EXISTING, WRITE, CREATE);
-		} catch (IOException cause) {
-			throw warn(new UncheckedIOException(cause), log);
-		}
-	}
+    private static BufferedWriter writerFor(Category category, File baseFolder)
+            throws UncheckedIOException {
+        Path path = new File(baseFolder, fileName(category)).toPath();
+        try {
+            return Files.newBufferedWriter(path, TRUNCATE_EXISTING, WRITE, CREATE);
+        } catch (IOException cause) {
+            throw warn(new UncheckedIOException(cause), log);
+        }
+    }
 
-	private static String fileName(Category category) {
-		return category.name() + extension;
-	}
+    private static String fileName(Category category) {
+        return category.name() + extension;
+    }
 
-	@Override
-	public void close() throws IOException {
-		writer.close();
-	}
+    @Override
+    public void close() throws IOException {
+        writer.close();
+    }
 
 }
