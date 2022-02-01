7103f2a22817d01e19a0bcfa34ad7a5a783d4347
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 17:02:01 +1100

    Add comment and reformat

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/DefaultActivityAssigner.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/DefaultActivityAssigner.java
@@ -14,47 +14,50 @@ import edu.kit.ifv.mobitopp.util.panel.PersonOfPanelDataId;
 
 public class DefaultActivityAssigner implements ActivityScheduleAssigner {
 
-  private final PanelDataRepository panelRepository;
-  private final ActivityScheduleCreator scheduleCreator;
-  private Map<PersonOfPanelDataId, TourBasedActivityPattern> cachedActivityPattern = new LinkedHashMap<>();
+    private final PanelDataRepository panelRepository;
+    private final ActivityScheduleCreator scheduleCreator;
+    private Map<PersonOfPanelDataId, TourBasedActivityPattern> cachedActivityPattern = new LinkedHashMap<>();
 
-  public DefaultActivityAssigner(
-      PanelDataRepository panelRepository, ActivityScheduleCreator scheduleCreator) {
-    this.panelRepository = panelRepository;
-    this.scheduleCreator = scheduleCreator;
-  }
-
-  private PersonOfPanelDataId createPersonPanelIdFor(
-      PersonBuilder person, HouseholdOfPanelDataId householdId) {
-    int personNumber = person.getId().getPersonNumber();
-    return new PersonOfPanelDataId(householdId, personNumber);
-  }
-
-  @Override
-  public void assignActivitySchedule(HouseholdForSetup household) {
-    short year = household.getId().getYear();
-    long householdNumber = household.getId().getHouseholdNumber();
-    HouseholdOfPanelDataId householdId = new HouseholdOfPanelDataId(year, householdNumber);
-    HouseholdOfPanelData panelHousehold = panelRepository.getHousehold(householdId);
-    for (PersonBuilder person : household.getPersons()) {
-      doAssignActivityProgramTo(person, householdId, panelHousehold, household);
+    public DefaultActivityAssigner(
+            PanelDataRepository panelRepository, ActivityScheduleCreator scheduleCreator) {
+        this.panelRepository = panelRepository;
+        this.scheduleCreator = scheduleCreator;
     }
-  }
 
-  private void doAssignActivityProgramTo(
-      PersonBuilder person, HouseholdOfPanelDataId householdId,
-      HouseholdOfPanelData panelHousehold, HouseholdForSetup household) {
-    PersonOfPanelDataId personId = createPersonPanelIdFor(person, householdId);
-    PersonOfPanelData panelPerson = panelRepository.getPerson(personId);
-    PatternActivityWeek activityPattern = scheduleCreator
-        .createActivitySchedule(panelPerson, panelHousehold, household);
-    TourBasedActivityPattern activitySchedule;
-    if (cachedActivityPattern.containsKey(panelPerson.getId())) {
-      activitySchedule = cachedActivityPattern.get(panelPerson.getId());
-    } else {
-      activitySchedule = TourBasedActivityPatternCreator.fromPatternActivityWeek(activityPattern);
-      cachedActivityPattern.put(panelPerson.getId(), activitySchedule);
+    private PersonOfPanelDataId createPersonPanelIdFor(
+            PersonBuilder person, HouseholdOfPanelDataId householdId) {
+        int personNumber = person.getId().getPersonNumber();
+        return new PersonOfPanelDataId(householdId, personNumber);
     }
-    person.setPatternActivityWeek(activitySchedule);
-  }
+
+    @Override
+    public void assignActivitySchedule(HouseholdForSetup household) {
+        //Gauge the persons from given household. Each of them assign activity.
+        short year = household.getId().getYear();
+        long householdNumber = household.getId().getHouseholdNumber();
+        HouseholdOfPanelDataId householdId = new HouseholdOfPanelDataId(year, householdNumber);
+        HouseholdOfPanelData panelHousehold = panelRepository.getHousehold(householdId);
+        for (PersonBuilder person : household.getPersons()) {
+            doAssignActivityProgramTo(person, householdId, panelHousehold, household);
+        }
+    }
+
+    private void doAssignActivityProgramTo(
+            PersonBuilder person, HouseholdOfPanelDataId householdId,
+            HouseholdOfPanelData panelHousehold, HouseholdForSetup household) {
+        PersonOfPanelDataId personId = createPersonPanelIdFor(person, householdId);
+        PersonOfPanelData panelPerson = panelRepository.getPerson(personId);
+
+        //Activity pattern for a week. Calling the createActivitySchedule method in DefaultActivityScheduleCreator.
+        PatternActivityWeek activityPattern = scheduleCreator
+                .createActivitySchedule(panelPerson, panelHousehold, household);
+        TourBasedActivityPattern activitySchedule;
+        if (cachedActivityPattern.containsKey(panelPerson.getId())) {
+            activitySchedule = cachedActivityPattern.get(panelPerson.getId());
+        } else {
+            activitySchedule = TourBasedActivityPatternCreator.fromPatternActivityWeek(activityPattern);
+            cachedActivityPattern.put(panelPerson.getId(), activitySchedule);
+        }
+        person.setPatternActivityWeek(activitySchedule);
+    }
 }
