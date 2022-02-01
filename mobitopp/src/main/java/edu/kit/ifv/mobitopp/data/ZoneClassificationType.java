c7072b3a5f65eb283e90273f86ed54cdd83ac48e
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 15:47:09 +1100

    Reformat

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/ZoneClassificationType.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/ZoneClassificationType.java
@@ -7,21 +7,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public enum ZoneClassificationType {
 
-	studyArea("STUDYAREA"), extendedStudyArea("EXTENDEDSTUDYAREA"), outlyingArea("OUTLYINGAREA");
+    studyArea("STUDYAREA"), extendedStudyArea("EXTENDEDSTUDYAREA"), outlyingArea("OUTLYINGAREA");
 
-	private final String name;
+    private final String name;
 
-	private ZoneClassificationType(String name) {
-		this.name = name;
-	}
+    private ZoneClassificationType(String name) {
+        this.name = name;
+    }
 
-	public static ZoneClassificationType getTypeFromString(String typeAsString) {
-		for (ZoneClassificationType type : values()) {
-			if (type.name.equals(typeAsString)) {
-				return type;
-			}
-		}
-		throw warn(new IllegalArgumentException("No type found for string: " + typeAsString), log);
-	}
+    public static ZoneClassificationType getTypeFromString(String typeAsString) {
+        for (ZoneClassificationType type : values()) {
+            if (type.name.equals(typeAsString)) {
+                return type;
+            }
+        }
+        throw warn(new IllegalArgumentException("No type found for string: " + typeAsString), log);
+    }
 
 }
