642d4389d55e909b192f6e69af9d4d0a5f462e4e
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 17:00:42 +1100

    Add opportunityFile to generate actual opportunities

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ContextBuilder.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ContextBuilder.java
@@ -45,232 +45,240 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ContextBuilder {
 
-  private final StopWatch performanceLogger;
-  private final PopulationSynthesisParser format;
-  private final AreaTypeRepository areaTypeRepository;
-  private final TypeMapping modeToType;
-  private PersonChanger personChanger;
-  private LanguageFactory languageFactory;
-  private Function<ImpedanceIfc, ImpedanceIfc> wrapImpedance;
+    private final StopWatch performanceLogger;
+    private final PopulationSynthesisParser format;
+    private final AreaTypeRepository areaTypeRepository;
+    private final TypeMapping modeToType;
+    private PersonChanger personChanger;
+    private LanguageFactory languageFactory;
+    private Function<ImpedanceIfc, ImpedanceIfc> wrapImpedance;
 
-  private WrittenConfiguration configuration;
-  private DynamicParameters experimentalParameters;
-  private ResultWriter resultWriter;
-  private VisumNetwork network;
-  private SimpleRoadNetwork roadNetwork;
-  private DataRepositoryForPopulationSynthesis dataRepository;
-  private Map<String, MobilityProviderCustomerModel> carSharing;
-  private File carEngineFile;
-  private DemographyData demographyData;
-	private StructuralData zonePropertiesData;
+    private WrittenConfiguration configuration;
+    private DynamicParameters experimentalParameters;
+    private ResultWriter resultWriter;
+    private VisumNetwork network;
+    private SimpleRoadNetwork roadNetwork;
+    private DataRepositoryForPopulationSynthesis dataRepository;
+    private Map<String, MobilityProviderCustomerModel> carSharing;
+    private File carEngineFile;
+    private File opportunityFile;
+    private DemographyData demographyData;
+    private StructuralData zonePropertiesData;
 
-	public ContextBuilder(
-			final AreaTypeRepository areaTypeReposirtory, final TypeMapping modeToType,
-			final LanguageFactory languageFactory, final PersonChanger personChanger) {
-		super();
-		areaTypeRepository = areaTypeReposirtory;
-		this.modeToType = modeToType;
-		this.languageFactory = languageFactory;
-		this.personChanger = personChanger;
-		this.wrapImpedance = Function.identity();
-		performanceLogger = new StopWatch(LocalDateTime::now);
-		ParserBuilder parser = new ParserBuilder();
-		format = parser.forPopulationSynthesis();
-	}
-	
-	public ContextBuilder(
-			AreaTypeRepository areaTypeReposirtory, TypeMapping modeToType,
-			LanguageFactory netfileLanguage) {
-		this(areaTypeReposirtory, modeToType, netfileLanguage, PersonChanger.noChange());
-	}
+    public ContextBuilder(
+            final AreaTypeRepository areaTypeReposirtory, final TypeMapping modeToType,
+            final LanguageFactory languageFactory, final PersonChanger personChanger) {
+        super();
+        areaTypeRepository = areaTypeReposirtory;
+        this.modeToType = modeToType;
+        this.languageFactory = languageFactory;
+        this.personChanger = personChanger;
+        this.wrapImpedance = Function.identity();
+        performanceLogger = new StopWatch(LocalDateTime::now);
+        ParserBuilder parser = new ParserBuilder();
+        format = parser.forPopulationSynthesis();
+    }
 
-	public ContextBuilder(AreaTypeRepository areaTypeRepository) {
-		this(areaTypeRepository, DefaultMappings.noAutonomousModes(), StandardNetfileLanguages::german);
-	}
+    public ContextBuilder(
+            AreaTypeRepository areaTypeReposirtory, TypeMapping modeToType,
+            LanguageFactory netfileLanguage) {
+        this(areaTypeReposirtory, modeToType, netfileLanguage, PersonChanger.noChange());
+    }
 
-	public ContextBuilder() {
-		this(new BicRepository(), DefaultMappings.noAutonomousModes(),
-				StandardNetfileLanguages::german);
-	}
-  
-  public ContextBuilder use(PersonChanger personChanger) {
-  	this.personChanger = personChanger;
-  	return this;
-  }
-  
-  public ContextBuilder use(LanguageFactory languageFactory) {
-  	this.languageFactory = languageFactory;
-  	return this;
-  }
-  
-  public ContextBuilder use(Function<ImpedanceIfc, ImpedanceIfc> wrapImpedance) {
-  	this.wrapImpedance = wrapImpedance;
-  	return this;
-  }
+    public ContextBuilder(AreaTypeRepository areaTypeRepository) {
+        this(areaTypeRepository, DefaultMappings.noAutonomousModes(), StandardNetfileLanguages::english);
+    }
 
-  public SynthesisContext buildFrom(File configurationFile) throws IOException {
-    WrittenConfiguration configuration = format.parse(configurationFile);
-    return buildFrom(configuration);
-  }
+    public ContextBuilder() {
+        this(new BicRepository(), DefaultMappings.noAutonomousModes(),
+                StandardNetfileLanguages::english);
+    }
 
-  public SynthesisContext buildFrom(WrittenConfiguration configuration) throws IOException {
-    this.configuration = configuration;
-    return loadData();
-  }
+    public ContextBuilder use(PersonChanger personChanger) {
+        this.personChanger = personChanger;
+        return this;
+    }
 
-  private SynthesisContext loadData() throws IOException {
-    startLoading();
-    validateConfiguration();
-    experimentalParameters();
-    resultWriter();
-    zoneProperties();
-    visumNetwork();
-    roadNetwork();
-    demography();
-    dataRepository();
-    carSharing();
-    carEngine();
-    SynthesisContext context = createContext();
-    printPerformance();
-    return context;
-  }
+    public ContextBuilder use(LanguageFactory languageFactory) {
+        this.languageFactory = languageFactory;
+        return this;
+    }
 
-  private void startLoading() {
-    performanceLogger.start();
-  }
+    public ContextBuilder use(Function<ImpedanceIfc, ImpedanceIfc> wrapImpedance) {
+        this.wrapImpedance = wrapImpedance;
+        return this;
+    }
 
-  private void log(String message) {
-    performanceLogger.measurePoint(message);
-  }
+    public SynthesisContext buildFrom(File configurationFile) throws IOException {
+        WrittenConfiguration configuration = format.parse(configurationFile);
+        return buildFrom(configuration);
+    }
 
-  private void printPerformance() {
-    log.info("Runtimes while loading context:");
-    performanceLogger.forEach((m, d) -> log.info(m + " " + d));
-  }
+    public SynthesisContext buildFrom(WrittenConfiguration configuration) throws IOException {
+        this.configuration = configuration;
+        return loadData();
+    }
 
-  private void validateConfiguration() throws IOException {
-    new Validate(modeToType).now(configuration);
-    log("Validate configuration");
-  }
+    private SynthesisContext loadData() throws IOException {
+        startLoading();
+        validateConfiguration();
+        experimentalParameters();
+        resultWriter();
+        zoneProperties();
+        visumNetwork();
+        roadNetwork();
+        demography();
+        dataRepository();
+        carSharing();
+        carEngine();
+        opportunity();
+        SynthesisContext context = createContext();
+        printPerformance();
+        return context;
+    }
 
-  private void experimentalParameters() {
-    experimentalParameters = new DynamicParameters(configuration.getExperimental());
-    log("Create experimental parameters");
-  }
+    private void startLoading() {
+        performanceLogger.start();
+    }
 
-  private void resultWriter() {
-    File inBaseFolder = Convert.asFile(configuration.getResultFolder());
-    resultWriter = ResultWriter.create(inBaseFolder);
-    log("Configure result writer");
-  }
-  
-  private void zoneProperties() {
-  	File file = Convert.asFile(configuration.getSynthesisZoneProperties());
-    zonePropertiesData = new StructuralData(CsvFile.createFrom(file));
-    log("Configure result writer");
-  }
+    private void log(String message) {
+        performanceLogger.measurePoint(message);
+    }
 
-  private void visumNetwork() {
-    network = loadNetwork();
-    log("Load visum network");
-  }
+    private void printPerformance() {
+        log.info("Runtimes while loading context:");
+        performanceLogger.forEach((m, d) -> log.info(m + " " + d));
+    }
 
-  protected VisumNetwork loadNetwork() {
-    File visumFile = Convert.asFile(configuration.getVisumFile());
-    String ptSystemCode = configuration.getVisumToMobitopp().getPtTransportSystemCode();
-		return new VisumNetworkReader(buildLanguage()).readNetwork(visumFile, ptSystemCode);
-  }
+    private void validateConfiguration() throws IOException {
+        new Validate(modeToType).now(configuration);
+        log("Validate configuration");
+    }
 
-	private NetfileLanguage buildLanguage() {
-		String carSystem = configuration.getVisumToMobitopp().getCarTransportSystemCode();
-		String individualWalkSystem = configuration.getVisumToMobitopp().getIndividualWalkTransportSystemCode();
-		String publicTransportWalkSystem = configuration.getVisumToMobitopp().getPtWalkTransportSystemCode();
-		StandardNetfileLanguages builder = StandardNetfileLanguages
-				.builder()
-				.carSystem(carSystem)
-				.individualWalkSystem(individualWalkSystem)
-				.publicTransportWalkSystem(publicTransportWalkSystem)
-				.build();
-		return languageFactory.createFrom(builder);
-	}
+    private void experimentalParameters() {
+        experimentalParameters = new DynamicParameters(configuration.getExperimental());
+        log("Create experimental parameters");
+    }
 
-  private void roadNetwork() {
-    roadNetwork = createRoadNetwork(network);
-    log("Create road network");
-  }
+    private void resultWriter() {
+        File inBaseFolder = Convert.asFile(configuration.getResultFolder());
+        resultWriter = ResultWriter.create(inBaseFolder);
+        log("Configure result writer");
+    }
 
-  protected SimpleRoadNetwork createRoadNetwork(VisumRoadNetwork network) {
-    return new SimpleRoadNetwork(network, carOf(network));
-  }
+    private void zoneProperties() {
+        File file = Convert.asFile(configuration.getSynthesisZoneProperties());
+        zonePropertiesData = new StructuralData(CsvFile.createFrom(file));
+        log("Configure zone properties");
+    }
 
-  private VisumTransportSystem carOf(VisumRoadNetwork network) {
-    VisumToMobitopp visumToMobitopp = configuration.getVisumToMobitopp();
-    String carCode = visumToMobitopp.getCarTransportSystemCode();
-    return network.transportSystems.getBy(carCode);
-  }
+    private void visumNetwork() {
+        network = loadNetwork();
+        log("Load visum network");
+    }
 
-  private void demography() {
-    Logger logger = zoneId -> log.warn("Missing zone with ID: " + zoneId);
-    demographyData = demographyData(network, logger);
-    log("Load demography data");
-  }
+    protected VisumNetwork loadNetwork() {
+        File visumFile = Convert.asFile(configuration.getVisumFile());
+        String ptSystemCode = configuration.getVisumToMobitopp().getPtTransportSystemCode();
+        return new VisumNetworkReader(buildLanguage()).readNetwork(visumFile, ptSystemCode);
+    }
 
-  private DemographyData demographyData(VisumNetwork network, Logger logger) {
-    DemographyDataBuilder builder = new DemographyDataBuilder(configuration);
-    DemographyData data = builder.build();
-    verify(data, network, logger);
-    log("Load demography data");
-    return data;
-  }
+    private NetfileLanguage buildLanguage() {
+        String carSystem = configuration.getVisumToMobitopp().getCarTransportSystemCode();
+        String individualWalkSystem = configuration.getVisumToMobitopp().getIndividualWalkTransportSystemCode();
+        String publicTransportWalkSystem = configuration.getVisumToMobitopp().getPtWalkTransportSystemCode();
+        StandardNetfileLanguages builder = StandardNetfileLanguages
+                .builder()
+                .carSystem(carSystem)
+                .individualWalkSystem(individualWalkSystem)
+                .publicTransportWalkSystem(publicTransportWalkSystem)
+                .build();
+        return languageFactory.createFrom(builder);
+    }
 
-	private void dataRepository() throws IOException {
-		int numberOfZones = configuration.getNumberOfZones();
-		DemandRegionMapping demandRegionMapping = loadDemandRegionMapping();
-		dataRepository = configuration
-				.getDataSource()
-				.forPopulationSynthesis(network, roadNetwork, demographyData, zonePropertiesData,
-						panelData(), numberOfZones, startDate(), resultWriter, areaTypeRepository, modeToType,
-						personChanger, wrapImpedance, demandRegionMapping);
-		log("Load data repository");
-	}
+    private void roadNetwork() {
+        roadNetwork = createRoadNetwork(network);
+        log("Create road network");
+    }
 
-	private DemandRegionMapping loadDemandRegionMapping() {
-		return new DemandRegionMappingFactory().create(configuration.getDemandRegionMapping());
-	}
+    protected SimpleRoadNetwork createRoadNetwork(VisumRoadNetwork network) {
+        return new SimpleRoadNetwork(network, carOf(network));
+    }
 
-	private void verify(DemographyData data, VisumNetwork network, Logger logger) {
-    List<Integer> zoneIds = network.zones.values().stream().map(zone -> zone.id).collect(toList());
-    new DemographyChecker(zoneIds, logger::println).check(data);
-  }
+    private VisumTransportSystem carOf(VisumRoadNetwork network) {
+        VisumToMobitopp visumToMobitopp = configuration.getVisumToMobitopp();
+        String carCode = visumToMobitopp.getCarTransportSystemCode();
+        return network.transportSystems.getBy(carCode);
+    }
 
-  private PanelDataRepository panelData() {
-    File panelDataFile = Convert.asFile(configuration.getPanelData());
-    PanelDataRepository panelData = LocalPanelDataRepository.loadFrom(panelDataFile);
-    log("Load panel data");
-    return panelData;
-  }
+    private void demography() {
+        Logger logger = zoneId -> log.warn("Missing zone with ID: " + zoneId);
+        demographyData = demographyData(network, logger);
+        log("Load demography data");
+    }
 
-  private StartDateSpecification startDate() {
-    return SimulationDays::simulationStart;
-  }
+    private DemographyData demographyData(VisumNetwork network, Logger logger) {
+        DemographyDataBuilder builder = new DemographyDataBuilder(configuration);
+        DemographyData data = builder.build();
+        verify(data, network, logger);
+        log("Load demography data");
+        return data;
+    }
 
-	private void carSharing() {
-		carSharing = new CarSharingParser(configuration.getMobilityProviders(), configuration.getSeed())
-				.parse();
-		log("Load car sharing");
-	}
+    private void dataRepository() throws IOException {
+        int numberOfZones = configuration.getNumberOfZones();
+        DemandRegionMapping demandRegionMapping = loadDemandRegionMapping(); //Demand region is null here.
+        //Here reads other data from the csv files.
+        dataRepository = configuration
+                .getDataSource()
+                .forPopulationSynthesis(network, roadNetwork, demographyData, zonePropertiesData,
+                        panelData(), numberOfZones, startDate(), resultWriter, areaTypeRepository, modeToType,
+                        personChanger, wrapImpedance, demandRegionMapping);
+        log("Load data repository");
+    }
 
-  private void carEngine() {
-    carEngineFile = Convert.asFile(configuration.getCarOwnership().getEngine());
-    log("Load car ownership model");
-  }
+    private DemandRegionMapping loadDemandRegionMapping() {
+        return new DemandRegionMappingFactory().create(configuration.getDemandRegionMapping());
+    }
 
-	private SynthesisContext createContext() {
-		Map<RegionalLevel, List<AttributeType>> attributes = demographyData.allAttributes();
-		SimpleSynthesisContext context = new SimpleSynthesisContext(configuration,
-				experimentalParameters, network, roadNetwork, dataRepository, carSharing, carEngineFile,
-				attributes, format, resultWriter);
-		log("Create context");
-		return context;
-	}
+    private void verify(DemographyData data, VisumNetwork network, Logger logger) {
+        List<Integer> zoneIds = network.zones.values().stream().map(zone -> zone.id).collect(toList());
+        new DemographyChecker(zoneIds, logger::println).check(data);
+    }
+
+    private PanelDataRepository panelData() {
+        File panelDataFile = Convert.asFile(configuration.getPanelData());
+        PanelDataRepository panelData = LocalPanelDataRepository.loadFrom(panelDataFile);
+        log("Load panel data");
+        return panelData;
+    }
+
+    private StartDateSpecification startDate() {
+        return SimulationDays::simulationStart;
+    }
+
+    private void carSharing() {
+        carSharing = new CarSharingParser(configuration.getMobilityProviders(), configuration.getSeed())
+                .parse();
+        log("Load car sharing");
+    }
+
+    private void carEngine() {
+        carEngineFile = Convert.asFile(configuration.getCarOwnership().getEngine());
+        log("Load car ownership model");
+    }
+
+    private void opportunity() {
+        opportunityFile = Convert.asFile(configuration.getPois());
+        log("Load opportunity file");
+    }
+
+    private SynthesisContext createContext() {
+        Map<RegionalLevel, List<AttributeType>> attributes = demographyData.allAttributes();
+        SimpleSynthesisContext context = new SimpleSynthesisContext(configuration,
+                experimentalParameters, network, roadNetwork, dataRepository, carSharing, carEngineFile, opportunityFile,
+                attributes, format, resultWriter);
+        log("Create context");
+        return context;
+    }
 }
