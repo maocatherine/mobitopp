47f2d83cb60c69084c062f888b13f3ec29c97d52
Lewen Feng <maocatherine93@gmail.com>
2022-02-01 15:35:27 +1100

    Reformat

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/activityschedule/Activity.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/activityschedule/Activity.java
@@ -10,247 +10,226 @@ import edu.kit.ifv.mobitopp.time.Time;
 
 public class Activity implements ActivityIfc {
 
-	private static final int maximumDuration = PatternActivity.maximumDuration;
+    private static final int maximumDuration = PatternActivity.maximumDuration;
 
-  private final int _oid;
-  private final byte activityNrOfWeek;
-  private final ActivityType activityType;
-  private Time startDate;
-  private Mode mode = null;
-  private boolean isRunning = false;
-  private int duration; // [in min]
-  private final int observedTripDuration; // [in min]
-	private ZoneAndLocation zoneAndLocation = null;
-	private Time currentEndDate;
-	
-	private final float startFlexibility;
-	private final float endFlexibility;
-	private final float durationFlexibility;
+    private final int _oid;
+    private final byte activityNrOfWeek;
+    private final ActivityType activityType;
+    private Time startDate;
+    private Mode mode = null;
+    private boolean isRunning = false;
+    private int duration; // [in min]
+    private final int observedTripDuration; // [in min]
+    private ZoneAndLocation zoneAndLocation = null;
+    private Time currentEndDate;
 
-  public Activity(
-		int oid,
-		byte activityNrOfWeek,
-		ActivityType activityType,
-		Time startDate,
-		int duration,
-		int observedTripDuration,
-		float startFlexibility,
-		float endFlexibility,
-		float durationFlexibility
-	)
-  {
-    this._oid = oid;
-		this.activityNrOfWeek=activityNrOfWeek;
-		this.activityType=activityType;
-		this.startDate=startDate;
+    private final float startFlexibility;
+    private final float endFlexibility;
+    private final float durationFlexibility;
 
-		verifyDuration(duration);
-		this.duration=duration;
+    public Activity(
+            int oid,
+            byte activityNrOfWeek,
+            ActivityType activityType,
+            Time startDate,
+            int duration,
+            int observedTripDuration,
+            float startFlexibility,
+            float endFlexibility,
+            float durationFlexibility
+    ) {
+        this._oid = oid;
+        this.activityNrOfWeek = activityNrOfWeek;
+        this.activityType = activityType;
+        this.startDate = startDate;
 
-		assert observedTripDuration <= maximumDuration;
-		this.observedTripDuration=observedTripDuration;
-		
-		this.startFlexibility = startFlexibility;
-		this.endFlexibility = endFlexibility;
-		this.durationFlexibility = durationFlexibility;
-		calculateCurrentEndDate();
-  }
-  
-  public Activity(
-  		int oid,
-  		byte activityNrOfWeek,
-  		ActivityType activityType,
-  		Time startDate,
-  		int duration,
-  		int observedTripDuration
-  	) {
-  	this(oid, activityNrOfWeek, activityType, startDate, duration, observedTripDuration, 1.0f, 0.0f, 1.0f);
-  }
-  
-  private void verifyDuration(int duration) {
-    assert duration >= 1 : duration;
-    assert duration <= 2 * maximumDuration : duration;
-  }
-  
-  private void calculateCurrentEndDate() {
-    currentEndDate = startDate().plusMinutes(duration());
-  }
+        verifyDuration(duration);
+        this.duration = duration;
 
-  public int getOid()
-  {
-    return this._oid;
-  }
+        assert observedTripDuration <= maximumDuration;
+        this.observedTripDuration = observedTripDuration;
 
-  public boolean isRunning()
-  {
-    return this.isRunning;
-  }
+        this.startFlexibility = startFlexibility;
+        this.endFlexibility = endFlexibility;
+        this.durationFlexibility = durationFlexibility;
+        calculateCurrentEndDate();
+    }
 
-  public Time startDate()
-  {
-		assert this.startDate!=null;
+    public Activity(
+            int oid,
+            byte activityNrOfWeek,
+            ActivityType activityType,
+            Time startDate,
+            int duration,
+            int observedTripDuration
+    ) {
+        this(oid, activityNrOfWeek, activityType, startDate, duration, observedTripDuration, 1.0f, 0.0f, 1.0f);
+    }
 
-    return this.startDate; 
-  }
+    private void verifyDuration(int duration) {
+        assert duration >= 1 : duration;
+        assert duration <= 2 * maximumDuration : duration;
+    }
 
-  public int duration()
-  {
-    return this.duration;
-  }
+    private void calculateCurrentEndDate() {
+        currentEndDate = startDate().plusMinutes(duration());
+    }
 
-  public int observedTripDuration()
-  {
-    return this.observedTripDuration;
-  }
+    public int getOid() {
+        return this._oid;
+    }
 
-  public void setMode(Mode mode)
-  {
-		assert mode != null;
+    public boolean isRunning() {
+        return this.isRunning;
+    }
 
-    this.mode = mode;
-  }
+    public Time startDate() {
+        assert this.startDate != null;
 
-  public void setRunning(boolean running_)
-  {
-    this.isRunning = running_;
-  }
+        return this.startDate;
+    }
 
-  public void setStartDate(Time date_)
-  {
-		assert date_ != null;
+    public int duration() {
+        return this.duration;
+    }
 
-    this.startDate = date_;
-    calculateCurrentEndDate();
-  }
+    public int observedTripDuration() {
+        return this.observedTripDuration;
+    }
 
-  public void changeDuration(int duration)
-  {
-		verifyDuration(duration);
+    public void setMode(Mode mode) {
+        assert mode != null;
 
-    this.duration = duration;
-    calculateCurrentEndDate();
-  }
+        this.mode = mode;
+    }
 
-  public ActivityType activityType()
-  {
-    return this.activityType;
-  }
+    public void setRunning(boolean running_) {
+        this.isRunning = running_;
+    }
 
-  public Time calculatePlannedEndDate()
-  {
-    return currentEndDate;
-  }
+    public void setStartDate(Time date_) {
+        assert date_ != null;
 
-  public boolean isLocationSet()
-  {
-    return this.zoneAndLocation != null;
-  }
-  
-  public boolean isModeSet()
-  {
-    return this.mode != null;
-  }
+        this.startDate = date_;
+        calculateCurrentEndDate();
+    }
 
-  public Mode mode()
-  {
-    return this.mode;
-  }
+    public void changeDuration(int duration) {
+        verifyDuration(duration);
 
-  public byte getActivityNrOfWeek() {
-  	return activityNrOfWeek;
-  }
+        this.duration = duration;
+        calculateCurrentEndDate();
+    }
+
+    public ActivityType activityType() {
+        return this.activityType;
+    }
+
+    public Time calculatePlannedEndDate() {
+        return currentEndDate;
+    }
+
+    public boolean isLocationSet() {
+        return this.zoneAndLocation != null;
+    }
+
+    public boolean isModeSet() {
+        return this.mode != null;
+    }
+
+    public Mode mode() {
+        return this.mode;
+    }
+
+    public byte getActivityNrOfWeek() {
+        return activityNrOfWeek;
+    }
 
 
-	public String toString() {
-					String s =
-						"Activity("
-						+ getOid() + ";"	
-						+ getActivityNrOfWeek() + ";"	
-						+	activityType() + ";"	
-						+ duration() + ";"	
-						+ observedTripDuration() + ";"	
-						+ (isLocationSet() ? zoneAndLocation.zone.getId().getExternalId() : "-") + ";"	
-						+ (isModeSet() ? mode() : "-") + ";"	
-						+ startDate().getDay() + ";"	
-						+ String.format("%1$02d:%2$02d", startDate().getHour(), startDate().getMinute()) + ";"
-						+ String.format("%1$02d:%2$02d", calculatePlannedEndDate().getHour(), 
-																						calculatePlannedEndDate().getMinute()) + ")";
+    public String toString() {
+        String s =
+                "Activity("
+                        + getOid() + ";"
+                        + getActivityNrOfWeek() + ";"
+                        + activityType() + ";"
+                        + duration() + ";"
+                        + observedTripDuration() + ";"
+                        + (isLocationSet() ? zoneAndLocation.zone.getId().getExternalId() : "-") + ";"
+                        + (isModeSet() ? mode() : "-") + ";"
+                        + startDate().getDay() + ";"
+                        + String.format("%1$02d:%2$02d", startDate().getHour(), startDate().getMinute()) + ";"
+                        + String.format("%1$02d:%2$02d", calculatePlannedEndDate().getHour(),
+                        calculatePlannedEndDate().getMinute()) + ")";
 
-		return s;
-	}
+        return s;
+    }
 
-	public void setLocation(ZoneAndLocation zoneAndLocation) {
-		assert zoneAndLocation != null;
-		assert this.zoneAndLocation == null;
+    public void setLocation(ZoneAndLocation zoneAndLocation) {
+        assert zoneAndLocation != null;
+        assert this.zoneAndLocation == null;
 
-		this.zoneAndLocation = zoneAndLocation;
-	}
+        this.zoneAndLocation = zoneAndLocation;
+    }
 
-	public Zone zone() {
-		assert this.zoneAndLocation != null : ("zone is null for activity of type " + activityType() + "\n" + this);
+    public Zone zone() {
+        assert this.zoneAndLocation != null : ("zone is null for activity of type " + activityType() + "\n" + this);
 
-		return this.zoneAndLocation.zone;
-	}
+        return this.zoneAndLocation.zone;
+    }
 
-	public Location location() {
-		assert this.zoneAndLocation != null;
+    public Location location() {
+        assert this.zoneAndLocation != null;
 
-		return this.zoneAndLocation.location;
-	}
-	
-	public ZoneAndLocation zoneAndLocation() {
-		assert this.zoneAndLocation != null;
+        return this.zoneAndLocation.location;
+    }
 
-		return this.zoneAndLocation;
-	}
+    public ZoneAndLocation zoneAndLocation() {
+        assert this.zoneAndLocation != null;
 
-
-
-	@Override
-	public boolean hasFixedDuration() {
-		
-		return durationFlexibility < 0.5f;
-	}
+        return this.zoneAndLocation;
+    }
 
 
+    @Override
+    public boolean hasFixedDuration() {
 
-	@Override
-	public boolean hasFixedStart() {
-		
-		return startFlexibility < 0.5f;
-	}
-
+        return durationFlexibility < 0.5f;
+    }
 
 
-	@Override
-	public boolean hasFixedEnd() {
-		
-		return endFlexibility < 0.5f;
-	}
+    @Override
+    public boolean hasFixedStart() {
+
+        return startFlexibility < 0.5f;
+    }
 
 
+    @Override
+    public boolean hasFixedEnd() {
 
-	@Override
-	public float durationFlexibility() {
-		
-		return durationFlexibility;
-	}
-
+        return endFlexibility < 0.5f;
+    }
 
 
-	@Override
-	public float startFlexibility() {
-		
-		return startFlexibility;
-	}
+    @Override
+    public float durationFlexibility() {
+
+        return durationFlexibility;
+    }
 
 
+    @Override
+    public float startFlexibility() {
 
-	@Override
-	public float endFlexibility() {
+        return startFlexibility;
+    }
 
-		return endFlexibility ;
-	}
+
+    @Override
+    public float endFlexibility() {
+
+        return endFlexibility;
+    }
 
 }
 
